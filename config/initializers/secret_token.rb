a944895e65ff5bdecf8ea9e5c5fe64f3df6ac4c2
Cecep Barkah <cecep@41studio.com>
2014-01-17 09:49:45 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cblog::Application.config.secret_key_base = '05acabf1c7d9c7b310254e783e5cb8a46addec9cf6709f1b7783be49113126431f104c29d13d0dd128a34fbea6241451ff167d8b4c8b573776dcabbe3d6d5150'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cblog::Application.config.session_store :cookie_store, key: '_cblog_session'
